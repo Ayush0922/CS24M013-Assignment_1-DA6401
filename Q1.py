88f57a168baaad83a4d49a499e6734ba9bfd3984
Ayushman Bhatt <theperfectionist0922@gmail.com>
2025-03-09 00:58:49 +0530

    Update Q1.py
    Display example images of each label using a matplotlib plt

--- Q1.py
+++ Q1.py
@@ -20,3 +20,23 @@ labels = [
     "Bag", 
     "High-Ankle Sneakers"
 ]
+
+# Set up a plot grid approx 10*10 will give a good view
+plt.figure(figsize=(10, 10))
+
+# Display one sample image for each category
+for label in range(10):
+    # Get the second image of each label
+    sample_idx = np.where(train_labels == label)[0][1]
+    sample_image = train_images[sample_idx]
+    
+    # Add subplot and display the image in  (2 rows, 5 columns) format
+    ax = plt.subplot(2, 5, label + 1)
+    ax.imshow(sample_image, cmap='gray')
+    ax.set_title(labels[label])
+    ax.axis("off")
+
+plt.tight_layout()
+plt.suptitle("Fashion-MNIST Sample Images", fontsize=16, fontweight='bold')
+plt.show()
+
