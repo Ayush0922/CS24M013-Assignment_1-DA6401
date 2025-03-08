088c48dc3c6d4efe147a5d32ce124ce43d280194
Ayushman Bhatt <theperfectionist0922@gmail.com>
2025-03-09 03:04:25 +0530

    Update Q2.py
    Import libraries and load data

--- Q2.py
+++ Q2.py
@@ -1,1 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.datasets import fashion_mnist
+
+# Step 1: Enter the Carnival Park (Load the Dataset)
+(carnival_images_train, carnival_labels_train), (carnival_images_test, carnival_labels_test) = fashion_mnist.load_data()
 
