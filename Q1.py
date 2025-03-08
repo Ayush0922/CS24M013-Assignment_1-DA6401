783d9f291f7fa6c90f05cc7b24087628fb65b037
Ayushman Bhatt <theperfectionist0922@gmail.com>
2025-03-09 00:54:17 +0530

    Update Q1.py
    Load MNIST data and set labels(to be used later)

--- Q1.py
+++ Q1.py
@@ -3,3 +3,20 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from keras.datasets import fashion_mnist
+
+# Load Fashion-MNIST dataset
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# Setting class labels
+labels = [
+    "Half-Tshirt", 
+    "Lower", 
+    "Full-Tshirt",
+    "Dress",
+    "Jacket", 
+    "Heels",
+    "Shirt", 
+    "Shoes", 
+    "Bag", 
+    "High-Ankle Sneakers"
+]
