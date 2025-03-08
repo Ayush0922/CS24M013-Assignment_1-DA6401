9f25cf0edb2847cf92b2a61263cdcc90647c4de2
Ayushman Bhatt <theperfectionist0922@gmail.com>
2025-03-09 03:07:59 +0530

    Update Q2.py
    Defining hidden layer neurons!

--- Q2.py
+++ Q2.py
@@ -5,3 +5,11 @@ from keras.datasets import fashion_mnist
 # Step 1: Enter the Carnival Park (Load the Dataset)
 (carnival_images_train, carnival_labels_train), (carnival_images_test, carnival_labels_test) = fashion_mnist.load_data()
 
+
+
+
+
+
+# Step 4: Set Up the Carnival Game (Initialize the Network)
+neurons_in_hidden_layers = [160, 160]  # Specify the number of neurons in each hidden layer
+
